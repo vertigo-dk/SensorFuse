d516a6332d543f1aa11128da2b3028a8b97e3976
Frederik Tollund Juutilainen <frederik.juutilainen@alexandra.dk>
2017-01-26 17:38:34 +0100

    commit before adjustments

---
+++ src/SoundObjects.h
@@ -0,0 +1,13 @@
+//
+//  SoundObjects.h
+//  WAVE_SensorFuse
+//
+//  Created by Frederik Tollund Juutilainen on 26/01/2017.
+//
+//
+
+#ifndef SoundObjects_h
+#define SoundObjects_h
+
+
+#endif /* SoundObjects_h */
